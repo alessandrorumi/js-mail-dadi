b7706de5029575f6ca709d412e56d0f0d1a08d20
alessandrorumi <rumi.alessandro21@gmail.com>
2023-11-22 17:20:57 +0100

    Feat: Struttura Es. Dadi con console.log

--- Dadi/script/myScript.js
+++ Dadi/script/myScript.js
@@ -1,1 +1,20 @@
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+// Generare un numero random da 1 a 6 (giocatore)
+const userRandomNumber = Math.floor(Math.random() * 6 + 1);
+console.log(userRandomNumber);
+
+// Generare un numero random da 1 a 6 (pc)
+const computerRandomNumber = Math.floor(Math.random() * 6 + 1);
+console.log(computerRandomNumber);
+
+// Confronto tra i due numeri random
+if (userRandomNumber > computerRandomNumber) {
+  console.log('Hai Vinto');
+
+} else if (userRandomNumber < computerRandomNumber) {
+  console.log('Hai Perso');
+
+} else {
+  console.log('Pareggio');
+}
