b0930ea2cf30852e267e3f9d5854afba9015d8e4
alessandrorumi <rumi.alessandro21@gmail.com>
2023-11-22 14:32:08 +0100

    Test mail JS con prompt (senza Html)

--- Mail/script/myScript.js
+++ Mail/script/myScript.js
@@ -1,1 +1,22 @@
 // Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+
+// Chiedi all’utente la sua email
+const userInput = prompt('Qual è la tua mail?');
+console.log(userInput);
+
+// controlla che sia nella lista di chi può accedere
+const mailList = ['tizio.tizio@gmail.com', 'caio.caio@gmail.com', 'test.test@gmail.com']
+console.log(mailList);
+
+for (let i = 0; i < mailList.length; i++) {
+  
+  if (userInput === mailList[i]) {
+    // stampa un messaggio appropriato sull’esito del controllo
+    console.log('test mail accettata con prompt riuscito!')
+  } else {
+    console.log('test mail rifiutata con prompt riuscito!')
+  }
+
+}
+
+
