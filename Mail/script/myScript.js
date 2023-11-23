863609e48623a81f8b38820b5bf1c2f72d3a63a8
alessandrorumi <rumi.alessandro21@gmail.com>
2023-11-23 08:56:26 +0100

    Optimize: JS Es. Mail e Style: CSS base

--- Mail/script/myScript.js
+++ Mail/script/myScript.js
@@ -2,6 +2,8 @@
 
 /*
 
+// PROMPT
+
 // Chiedi all’utente la sua email
 const userInput = prompt('Qual è la tua mail?');
 
@@ -16,18 +18,18 @@ for (let i = 0; i < mailList.length; i++) {
     // stampa un messaggio appropriato sull’esito del controllo
     foundedMail = true;
   }
+  
+}
 
-  if (foundedMail === true) {
-    console.log('YES')
-  } else {
-    console.log('NO')
-  }
-
+if (foundedMail) {
+  console.log('YES')
+} else {
+  console.log('NO')
 }
 
 */
 
-// TEST CON INPUT TYPE TEXT E BUTTON
+// INPUT TYPE TEXT E BUTTON
 
 // Dichiaro l'input Mail
 const userMail = document.getElementById('email');
@@ -35,37 +37,55 @@ const userMail = document.getElementById('email');
 // Dichiaro il bottone "Verifica Email"
 const verify = document.getElementById('verifyEmail');
 
-// Dichiaro lo span di validità della Mail
-const textOutput = document.getElementById('textOutput');
+// Dichiaro il bottone "Verifica Email"
+const undo = document.getElementById('undoEmail');
+
+// Dichiaro lo span (Mail output a schermo)
+const output = document.getElementById('textOutput');
 
 // Array delle Mail 
-const mailList = ['tizio.tizio@gmail.com', 'caio.caio@gmail.com', 'test.test@gmail.com']
+const mailList = ['tizio.tizio@gmail.com', 'caio.caio@gmail.com', 'test.test@gmail.com', 'tizio.caio@gmail.com']
+console.log(mailList);
 
-// Definisco la variabile (in caso di corrispondenza Mail-Array)
-let foundedMail;
-
-// Al click del bottone genera
+// Al click del bottone Verifica Mail:
 verify.addEventListener('click',
 
   function() {
-
+    // Associo ad una variabile il valore della Mail
     userInput = userMail.value;
+
+    // Definisco la variabile (in caso di corrispondenza Mail-Array)
+    let foundedMail = false; // AAA VA IMPOSTATA 'FALSE' ALTRIMENTI ALLE ITERAZIONI SUCCESSIVE RIMARRA' IN MEMORIA IL RISULTATO 'TRUE'
 
     for (let i = 0; i < mailList.length; i++) {
   
       if (userInput === mailList[i]) {
-        // stampa un messaggio appropriato sull’esito del controllo
+        // Associo il risultato alla variabile (se facessi il console.log adeso risulterebbero 1 true e 2 false)
         foundedMail = true;
       }
-    
-      if (foundedMail === true) {
-        document.getElementById('textOutput').innerHTML = 'La tua Mail è corretta, puoi accedere!';
-      } else {
-        document.getElementById('textOutput').innerHTML = 'La tua Mail è sbagliata, non puoi accedere!';
-      }
-    
+      
     }    
-
+// Do un output alla variabile
+    if (foundedMail) {
+      // Positivo
+      output.innerHTML = 'La tua Mail è presente nella lista, puoi accedere!';
+    } else {
+      // Negativo
+      output.innerHTML = 'La tua Mail non è presente nella lista, accesso negato!';
+    }
+    
   }
 
-)
+);
+
+// Al click del bottone Annulla:
+undo.addEventListener('click',
+
+  function() {
+    // Rimuovi Parametri Inseriti
+    userMail.value = '';
+
+    output.innerHTML = '';
+  }
+
+);
